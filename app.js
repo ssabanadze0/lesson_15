0eb7b6041b214659dafd4e0ca7b64438563e71a2
ssabanadze0 <ssabanadze01@gmail.com>
2025-06-16 19:51:13 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("hello");
+// Primitive types
+
+// Number
+let age = 25;
+
+// String
+let city = "Tbilisi";
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let score;
+
+// Null
+let myNull = null;
+
+// Symbol
+let uniqueId = Symbol("id");
+
+// BigInt
+let bigNumber = 1234567890123456789012345678901234567890n;
+
+// Console log to test
+console.log("Age:", age);
+console.log("City:", city);
+console.log("Is student:", isStudent);
+console.log("Score:", score);
+console.log("Selected item:", myNull);
+console.log("Unique ID:", uniqueId);
+console.log("Big number:", bigNumber);
+// sentance
+const myname = "Saba";
+const dayOfWeek = "Monday";
+const favoriteActivity = "coding";
+let sentence1 =
+  "Hi, my name is " +
+  myname +
+  "! Today is " +
+  dayOfWeek +
+  ", a perfect day for " +
+  favoriteActivity +
+  ".";
+console.log(sentence1);
+let sentence2 = `Hi, my name is ${myname}! Today is ${dayOfWeek}, a perfect day for ${favoriteActivity}.`;
+console.log(sentence2);
